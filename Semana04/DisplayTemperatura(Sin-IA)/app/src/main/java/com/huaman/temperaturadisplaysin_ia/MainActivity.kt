6e7040f586588c4d57c9b967379e354b1999bf39
James Huaman <james.huaman@tecsup.edu.pe>
2026-09-15 21:55:49 -0500

    Agregar boton para subir temperatura

--- Semana04/DisplayTemperatura(Sin-IA)/app/src/main/java/com/huaman/temperaturadisplaysin_ia/MainActivity.kt
+++ Semana04/DisplayTemperatura(Sin-IA)/app/src/main/java/com/huaman/temperaturadisplaysin_ia/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,5 +42,13 @@ fun TemperatureDisplay() {
         Text(
             text = "Temperatura: $temperatura °C"
         )
+
+        Button(
+            onClick = {
+                temperatura++
+            }
+        ) {
+            Text("Subir")
+        }
     }
 }
