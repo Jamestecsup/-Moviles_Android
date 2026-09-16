208595175d3eb1fc6b1a87989b373cc7c4b1618b
James Huaman <james.huaman@tecsup.edu.pe>
2026-09-16 13:26:43 -0500

    Proyecto Inicial

---
+++ Semana04/Lab04CarritoTecsup/app/src/main/java/com/huaman/lab04_carrito_huaman/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.huaman.lab04_carrito_huaman
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.huaman.lab04_carrito_huaman.ui.theme.Lab04carritohuamanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab04carritohuamanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab04carritohuamanTheme {
+        Greeting("Android")
+    }
+}
