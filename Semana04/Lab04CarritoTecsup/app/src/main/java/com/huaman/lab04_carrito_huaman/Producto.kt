ceb6540b1871e8ad575d75a3dd711b68390eef4f
James Huaman <james.huaman@tecsup.edu.pe>
2026-09-16 14:05:52 -0500

    Avance Lazycolumn Inicial

--- Semana04/Lab04CarritoTecsup/app/src/main/java/com/huaman/lab04_carrito_huaman/Producto.kt
+++ Semana04/Lab04CarritoTecsup/app/src/main/java/com/huaman/lab04_carrito_huaman/Producto.kt
@@ -1,5 +1,6 @@
 package com.huaman.lab04_carrito_huaman
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,13 +13,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+
 data class Producto(
     val nombre: String,
     val precio: Double,
@@ -84,7 +92,19 @@ fun PantallaCarrito() {
         ) {
             Text("AGREGAR")
         }
-
         Text("Productos: ${productos.size}")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(productos) { producto ->
+                TarjetaProducto(
+                    producto = producto,
+                    onEliminar = { productos.remove(producto) }
+                )
+            }
+        }
     }
 }
